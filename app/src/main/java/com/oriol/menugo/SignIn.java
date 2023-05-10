dec61353dca1bde7951536e3c337aa37d48b62ca
ElSrCata <92938293+ElSrCata@users.noreply.github.com>
2023-05-10 19:18:41 +0200

    Register&Login

---
+++ app/src/main/java/com/oriol/menugo/SignIn.java
@@ -0,0 +1,86 @@
+package com.oriol.menugo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.oriol.menugo.Model.User;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignIn extends AppCompatActivity {
+
+    EditText edtPhone, edtPassword;
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText) findViewById(R.id.edtPhone);
+
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        //Iniciamos Firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://menugo-9451c-default-rtdb.europe-west1.firebasedatabase.app/");
+        DatabaseReference user_table = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Espera...");
+                mDialog.show();
+
+                user_table.addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        //Comprueba si el usuario existe en la BBDD
+                        if (dataSnapshot.child(edtPhone.getText().toString()).exists())
+                        {
+                            //Cojemos la información del usuario
+                            mDialog.dismiss();
+                            User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+
+                            if (user.getPassword().equals(edtPassword.getText().toString()))
+                            {
+                                Toast.makeText(SignIn.this, "Logueado correctamente", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(SignIn.this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this, "El usuario no existe", Toast.LENGTH_SHORT).show();
+                        }
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/oriol/menugo/SignUp.java
@@ -0,0 +1,76 @@
+package com.oriol.menugo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.oriol.menugo.Model.User;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+public class SignUp extends AppCompatActivity {
+
+    MaterialEditText edtPhone, edtName, edtPassword;
+    Button btnSignUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        edtName = (MaterialEditText) findViewById(R.id.edtName);
+        edtPassword = (MaterialEditText) findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText) findViewById(R.id.edtPhone);
+
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        //Iniciamos Firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance("https://menugo-9451c-default-rtdb.europe-west1.firebasedatabase.app/");
+        final DatabaseReference user_table = database.getReference("User");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final ProgressDialog mDialog = new ProgressDialog(SignUp.this);
+                mDialog.setMessage("Espera...");
+                mDialog.show();
+
+                user_table.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                    //Comprobamos si el teléfono ya está en uso
+
+                        if(dataSnapshot.child(edtPhone.getText().toString()).exists())
+                        {
+                            mDialog.dismiss();
+                            Toast.makeText(SignUp.this, "El teléfono ya está en uso", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            mDialog.dismiss();
+                            User user = new User(edtName.getText().toString(), edtPassword.getText().toString());
+                            user_table.child(edtPhone.getText().toString()).setValue(user);
+                            Toast.makeText(SignUp.this, "Se ha registrado correctamente", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+
+    }
+}
