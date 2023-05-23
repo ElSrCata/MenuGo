61c1b73ec6dff8d080abd5942c5e10cfc15bf6f9
ElSrCata <92938293+ElSrCata@users.noreply.github.com>
2023-05-23 18:57:57 +0200

    Funcionalidad menú y modificar carrito

--- app/src/main/java/com/oriol/menugo/Database/Database.java
+++ app/src/main/java/com/oriol/menugo/Database/Database.java
@@ -24,7 +24,7 @@ public class Database extends SQLiteAssetHelper {
         SQLiteDatabase db = getReadableDatabase();
         SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
 
-        String[] sqlSelect = {"ProductName", "ProductId", "Quantity", "Price", "Discount"};
+        String[] sqlSelect = {"ID", "ProductName", "ProductId", "Quantity", "Price", "Discount"};
         String sqlTable = "OrderDetail";
 
         qb.setTables(sqlTable);
@@ -36,7 +36,9 @@ public class Database extends SQLiteAssetHelper {
         if(c.moveToFirst())
         {
             do {
-                result.add(new Order(c.getString(c.getColumnIndexOrThrow("ProductId")),
+                result.add(new Order(
+                        c.getInt(c.getColumnIndexOrThrow("ID")),
+                        c.getString(c.getColumnIndexOrThrow("ProductId")),
                         c.getString(c.getColumnIndexOrThrow("ProductName")),
                         c.getString(c.getColumnIndexOrThrow("Quantity")),
                         c.getString(c.getColumnIndexOrThrow("Price")),
@@ -114,5 +116,13 @@ public class Database extends SQLiteAssetHelper {
             } while (cursor.moveToNext());
         }
         return count;
+    }
+
+    public void updateCart(Order order) {
+        SQLiteDatabase db = getReadableDatabase();
+        String query = String.format("UPDATE OrderDetail SET Quantity %s WHERE ID = %d", order.getQuantity(), order.getID());
+        db.execSQL(query);
+
+
     }
 }
